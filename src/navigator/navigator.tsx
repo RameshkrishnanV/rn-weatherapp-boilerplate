10da35dc9764b52602dd134f917aa441f4f59446
ramesh.krishnan <ramesh.krishnan@zeasn.com>
2023-08-08 22:09:18 +0530

    tab navigator icon added

--- src/navigator/navigator.tsx
+++ src/navigator/navigator.tsx
@@ -3,16 +3,48 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/home/Home';
 import Forecast from '../screens/forecast/forecast';
+import { Image, View } from 'react-native';
+import Styles from '../styles/styles';
 
 const Tab = createBottomTabNavigator();
+
+const getTabIcon = (tabName: string, focused: boolean) => {
+  return (
+    <View>
+      <Image
+        source={
+          tabName === 'Home'
+            ? require('../assets/images/hot.png')
+            : require('../assets/images/weather-app.png')
+        }
+        style={[
+          Styles.tabIconStyle,
+          focused ? Styles.activeTab : Styles.inActiveTab,
+        ]}
+      />
+    </View>
+  );
+};
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName={'Home'}
       screenOptions={{ headerShown: false }}>
-      <Tab.Screen name={'Home'} component={Home} />
-      <Tab.Screen name={'Forecast'} component={Forecast} />
+      <Tab.Screen
+        name={'Home'}
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => getTabIcon('Home', focused),
+        }}
+      />
+      <Tab.Screen
+        name={'Forecast'}
+        component={Forecast}
+        options={{
+          tabBarIcon: ({ focused }) => getTabIcon('Forecast', focused),
+        }}
+      />
     </Tab.Navigator>
   );
 };
---
+++ src/styles/styles.ts
@@ -0,0 +1,17 @@
+import { ImageStyle } from 'react-native';
+
+const Styles = {
+  tabIconStyle: {
+    width: 22,
+    height: 22,
+    resizeMode: 'contain',
+  } as ImageStyle,
+  activeTab: {
+    opacity: 1,
+  },
+  inActiveTab: {
+    opacity: 0.5,
+  },
+};
+
+export default Styles;
