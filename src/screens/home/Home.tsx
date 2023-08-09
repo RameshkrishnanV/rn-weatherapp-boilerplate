358e0e2e4eb038a818fc5f06fe8447c0e6edc701
ramesh.krishnan <ramesh.krishnan@zeasn.com>
2023-08-09 16:40:47 +0530

    Integrated redux state management

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,7 +1,15 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { appLoaded } from '../../core/redux/actions/appActions';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(appLoaded());
+  });
+
   return (
     <View>
       <Text>Home</Text>
